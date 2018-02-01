739b32b95c7c17fc5b79d2059654f4d692b582ff
David Desberg <david.desberg@microsoft.com>
2018-02-01 14:06:12 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,57 +1,49 @@
-# Project Name
+---
+services: key-vault
+platforms: dotnet
+author: dadesber
+---
 
-(short, 1-3 sentenced, description of the project)
+# Authentication sample for Azure Key Vault using the Azure Node SDK
 
-## Features
-
-This project framework provides the following features:
-
-* Feature 1
-* Feature 2
-* ...
-
-## Getting Started
-
-### Prerequisites
-
-(ideally very short, if any)
-
-- OS
-- Library version
-- ...
-
-### Installation
-
-(ideally very short)
-
-- npm install [package name]
-- mvn install
-- ...
-
-### Quickstart
-(Add steps to get up and running quickly)
-
-1. git clone [repository clone url]
-2. cd [respository name]
-3. ...
+This sample repo demonstrates how to connect and authenticate to an Azure Key Vault vault. 
+To do so, it first uses the Key Vault Management Client to create a vault.
+The Key Vault client is then used to authenticate to the vault and set/retrieve a sample secret. 
 
 
-## Demo
+## How to run this sample
 
-A demo app is included to show how to use the project.
+1. If you don't already have it, get and install Visual Studio. 
 
-To run the demo, follow these steps:
+2. Clone the repo.
 
-(Add steps to start up the demo)
+   ```
+   git clone https://github.com/Azure-Samples/key-vault-dotnet-authentication.git key-vault
+   ```
 
-1.
-2.
-3.
+3. Create an Azure service principals, using one of the following:
+   - [Azure CLI](https://azure.microsoft.com/documentation/articles/resource-group-authenticate-service-principal-cli/),
+   - [PowerShell](https://azure.microsoft.com/documentation/articles/resource-group-authenticate-service-principal/)
+   - [Azure Portal](https://azure.microsoft.com/documentation/articles/resource-group-create-service-principal-portal/). 
 
-## Resources
+    This service principal is to run the sample on your azure account.
 
-(Any additional resources or related projects)
+4. Set the variables in App.config using the information from the service principal that you created.
 
-- Link to supporting information
-- Link to similar sample
-- ...
+   ```
+   AZURE_SUBSCRIPTION_ID={your subscription id}
+   AZURE_CLIENT_ID={your client id}
+   AZURE_CLIENT_SECRET={your client secret}
+   AZURE_TENANT_ID={your tenant id as a GUID}
+   AZURE_CLIENT_OID={Object id of the service principal}
+   ```
+
+5. Open the sample solution in Visual Studio. Build and run. 
+
+## References and further reading
+
+- [Azure SDK for .NET](https://github.com/Azure/azure-sdk-for-net)
+- [Azure KeyVault Documentation](https://azure.microsoft.com/en-us/documentation/services/key-vault/)
+- [Key Vault REST API Reference](https://msdn.microsoft.com/en-us/library/azure/dn903609.aspx)
+- [Manage Key Vault using CLI](https://azure.microsoft.com/en-us/documentation/articles/key-vault-manage-with-cli/)
+- [Storing and using secrets in Azure](https://blogs.msdn.microsoft.com/dotnet/2016/10/03/storing-and-using-secrets-in-azure/)
---
+++ key-vault-dotnet-authentication/KeyVaultAuthSample.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.KeyVault.Fluent;
+using Microsoft.Azure.Management.KeyVault.Fluent.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Models;
+
+namespace key_vault_dotnet_authentication
+{
+    class KeyVaultAuthSample
+    {
+        // Callback for authenticating to Key Vault using ADAL
+        public async Task<string> ADALGetTokenCallback(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(Properties.Settings.Default.AZURE_CLIENT_ID, Properties.Settings.Default.AZURE_CLIENT_SECRET);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+            if(result == null)
+            {
+                throw new InvalidOperationException("Failed to retrieve access token for Key Vault");
+            }
+
+            return result.AccessToken;
+        }
+
+        private async Task authUsingADALCallback(string vaultBaseURL)
+        {
+            Console.WriteLine("Authenticating to Key Vault using ADAL callback.");
+            Console.WriteLine(vaultBaseURL);
+            // Set up a KV Client with the above ADAL callback method
+            KeyVaultClient kvClient = new KeyVaultClient(ADALGetTokenCallback);
+
+            // Set and get an example secret
+            await kvClient.SetSecretAsync(vaultBaseURL, "test-secret", "test-secret-value-using-adal");
+            SecretBundle s = await kvClient.GetSecretAsync(vaultBaseURL, "test-secret");
+            Console.WriteLine("Retrieved \"test-secret\", value=\"" + s.Value + "\"");
+        }
+
+        public async Task run()
+        {
+            // Generate a random name for a new vault
+            String vaultName = Util.generateRandomVaultId();
+
+            // Set up credentials to access management plane to set up example key vault
+            AzureCredentials credentials = new AzureCredentialsFactory().FromServicePrincipal(
+                Properties.Settings.Default.AZURE_CLIENT_ID,
+                Properties.Settings.Default.AZURE_CLIENT_SECRET,
+                Properties.Settings.Default.AZURE_TENANT_ID,
+                AzureEnvironment.AzureGlobalCloud
+            );
+
+
+            // Ensure that our sample resource group exists. 
+            Console.WriteLine("Creating sample resource group");
+            ResourceManagementClient resourceMgmtClient = new ResourceManagementClient(credentials);
+            resourceMgmtClient.SubscriptionId = Properties.Settings.Default.AZURE_SUBSCRIPTION_ID;
+
+            await resourceMgmtClient.ResourceGroups.CreateOrUpdateAsync(Properties.Settings.Default.AZURE_RESOURCE_GROUP, new ResourceGroupInner(Properties.Settings.Default.AZURE_LOCATION));
+
+            // Create the sample key vault.
+            Console.WriteLine("Creating sample Key Vault - " + vaultName);
+
+            // Set up the params for the API call
+            VaultCreateOrUpdateParametersInner createParams = new VaultCreateOrUpdateParametersInner(
+                Properties.Settings.Default.AZURE_LOCATION,
+                new VaultProperties(
+                    Guid.Parse(Properties.Settings.Default.AZURE_TENANT_ID),
+                    new Microsoft.Azure.Management.KeyVault.Fluent.Models.Sku(SkuName.Standard),
+
+                    // Create an access policy granting all secret permissions to our sample's service principal
+                    new[] { new AccessPolicyEntry(Guid.Parse(Properties.Settings.Default.AZURE_TENANT_ID), Properties.Settings.Default.AZURE_CLIENT_OID, new Permissions(null, new[] { "all" })) }
+                )
+            );
+
+            KeyVaultManagementClient kvMgmtClient = new KeyVaultManagementClient(credentials);
+            kvMgmtClient.SubscriptionId = Properties.Settings.Default.AZURE_SUBSCRIPTION_ID;
+
+            VaultInner vault = await kvMgmtClient.Vaults.CreateOrUpdateAsync(Properties.Settings.Default.AZURE_RESOURCE_GROUP, vaultName, createParams);
+
+            // Now demo authentication to the vault using ADAL
+            // Add a delay to wait for KV DNS record to be created. See: https://github.com/Azure/azure-sdk-for-node/pull/1938
+            System.Threading.Thread.Sleep(5000);
+
+            await authUsingADALCallback(vault.Properties.VaultUri);
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Azure Key Vault Authentication Sample");
+            KeyVaultAuthSample sample = new KeyVaultAuthSample();
+            sample.run().Wait();
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
